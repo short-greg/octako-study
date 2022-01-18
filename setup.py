2adc643b64754cb729f4d0d965886212c39e0637
Greg Short <shortg29@gmail.com>
2022-01-18 18:39:09 +0900

    add installation script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='takostudy',
+    version='0.1.0',
+    packages=['takostudy']
+)
